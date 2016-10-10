d93189371ea9ec2b66d67af939f37c83492dccd1
Jack-Tomas-Liu <liuxinxian0806@gmail.com>
2016-10-10 17:16:43 +0800

    local app receive webview give argument

--- EventInjectToSystemApp/myapplication/src/main/java/com/sj/myapplication/MainActivity.java
+++ EventInjectToSystemApp/myapplication/src/main/java/com/sj/myapplication/MainActivity.java
@@ -1,13 +1,32 @@
 package com.sj.myapplication;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView mContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContent = (TextView)findViewById(R.id.id_content);
+        Intent intent = getIntent();
+        if(intent!=null){
+            String action = intent.getAction();
+            if(Intent.ACTION_VIEW.equals(action)){
+                Uri uri = intent.getData();
+                if(uri!=null){
+                    String arg = uri.getQueryParameter("arg");
+                    if(!TextUtils.isEmpty(arg)){
+                        mContent.setText(arg);
+                    }
+                }
+            }
+        }
     }
 }
